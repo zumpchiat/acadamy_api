e753f5efb8b6d7da6eeec5ee7e90a736f4f49ac5
zumpchiat <zumpchiat@gmail.com>
2025-10-18 21:47:51 -0300

    feat: add path paraments

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#Endpoint basic
 @app.get("/")
 def read_root():
     return {"message" : "My First api in FastAPI"}
+
+
+# Path Parameters
+@app.get("/greet/{username}")
+async def greet(username:str):
+    return {"message": f"Hello {username}"}
+
