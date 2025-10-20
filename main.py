f34141d643c65f210120fcd1a865077b5450274d
zumpchiat <zumpchiat@gmail.com>
2025-10-20 11:38:43 -0300

    feat: add  query paraments

--- main.py
+++ main.py
@@ -13,3 +13,20 @@ def read_root():
 async def greet(username:str):
     return {"message": f"Hello {username}"}
 
+# Query Parameters
+
+user_list = [
+    "João",
+    "Maria",
+    "josé",
+    "Pedro",
+]
+
+@app.get("/search")
+async def search(username:str):
+    for user in user_list:
+        if username in user_list:
+            return {"message": f" Seu usuário é {username}"}
+        
+        else:
+            return {"message": f"Usuário não encontrado!!!"} 
