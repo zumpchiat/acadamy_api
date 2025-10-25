f28c8abe1f7586149132c5a74aed6c1dfc1e871f
zumpchiat <zumpchiat@gmail.com>
2025-10-25 20:29:50 -0300

    feat: add endpoints academia

--- main.py
+++ main.py
@@ -1,68 +1,65 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,status
+from fastapi.exceptions import HTTPException
+from pydantic import BaseModel
+from typing import Optional,List
+
 
 app = FastAPI()
 
-#Endpoint basic
-@app.get("/")
-def read_root():
-    return {"message" : "My First api in FastAPI"}
-
-
-# Path Parameters
-@app.get("/greet/{username}")
-async def greet(username:str):
-    return {"message": f"Hello {username}"}
-
-# Query Parameters
-# http://localhost:8000/search?username=Pedro
-
-user_list = [
-    "João",
-    "Maria",
-    "josé",
-    "Pedro",
+academias = [{
+    "id": 1,
+    "nome": "Team Neves",
+    "responsavel_tecnico": "Nelson Gonçalves",
+    "fundacao": "1990",
+    "criado_at" : "2014-06-15" 
+},
+{
+    "id": 2,
+    "nome": "Team Caveira",
+    "responsavel_tecnico": "Anderson Almeida",
+    "fundacao": "2013",
+    "criado_at" : "2016-04-05" 
+},
+{
+    "id": 3,
+    "nome": "Team Carneiro",
+    "responsavel_tecnico": "Marcos Carneiro",
+    "fundacao": "2018",
+    "criado_at" : "2018-08-12"
+}
 ]
+class Academia(BaseModel):
+     id: int
+     nome : str
+     responsavel_tecnico : str
+     fundacao : str
+     criado_at : str
 
-@app.get("/search")
-async def search(username:str):
-    for user in user_list:
-        if username in user_list:
-            return {"message": f" Seu usuário é {username}"}
+# Get /api/academias
+@app.get("/api/academias", response_model=List[Academia])
+async def get_all_academias():
+    return academias
+    
+
+# Get /api/academias/id
+@app.get("/api/academias/{id}")
+async def get_academia(id:int) -> dict:
+    for academia in academias:     
+        if academia["id"] == id:
+            return academia
         
-        else:
-            return {"message": f"Usuário não encontrado!!!"} 
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Academia não encontrada!")
+
+# Post /api/academias
+@app.post("/api/academias/", status_code=status.HTTP_201_CREATED)
+async def create_academia(academia_data: Academia) -> dict:
+    new_academia = academia_data.model_dump()
+
+    academias.append(new_academia)
+
+    return new_academia
 
 
-#Optional Parameters
-
-from typing import Optional
-
-@app.get("/greetoptional")
-async def greetoptional(username:Optional[str]="Convidado"):
-    return {"message": f"Olá, {username}  como você está"}
 
 
 
-#Request Body
-
-from pydantic import BaseModel
-
-users =[]
-class UserSchema(BaseModel):
-    username: str
-    email: str
-
-@app.post("/create_user")
-async def create_user(user_data:UserSchema):
-    new_user = {
-        "username" : user_data.username,
-        "email": user_data.email
-    }
-
-    users.append(new_user)
-    return {"message": "Create sucessul", "user": new_user}
-
-
-@app.get("/user")
-async def user():
-    return {"message": f"{users}"}
