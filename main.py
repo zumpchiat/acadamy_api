786ea6c60a8bb3ae3de5e81ae17397ef2a35a9b7
zumpchiat <zumpchiat@gmail.com>
2025-10-25 17:50:41 -0300

    feat:  post parameters

--- main.py
+++ main.py
@@ -40,3 +40,29 @@ from typing import Optional
 @app.get("/greetoptional")
 async def greetoptional(username:Optional[str]="Convidado"):
     return {"message": f"Olá, {username}  como você está"}
+
+
+
+#Request Body
+
+from pydantic import BaseModel
+
+users =[]
+class UserSchema(BaseModel):
+    username: str
+    email: str
+
+@app.post("/create_user")
+async def create_user(user_data:UserSchema):
+    new_user = {
+        "username" : user_data.username,
+        "email": user_data.email
+    }
+
+    users.append(new_user)
+    return {"message": "Create sucessul", "user": new_user}
+
+
+@app.get("/user")
+async def user():
+    return {"message": f"{users}"}
